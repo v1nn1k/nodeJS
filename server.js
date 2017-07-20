84dfa9d2d676da4e8bf76f8754c06e11f10647a4
Ivan Vynnyk <smilecuber@gmail.com>
2017-07-20 12:12:24 +0200

    Server func

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
+
 const http = require('http').Server(app);
 
+let io = require('socket.io')(http);
 
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
-})
+
+app.use(express.static('public'));
+
+
 
 http.listen(3000, function() {
     console.log('listening on *:3000');
+});
+
+io.on('connection', function(socket) {
+    console.log('New user connected');
+
+    socket.on('message', function(data) {
+        io.emit('new message', data);
+
+    });
+
 });
